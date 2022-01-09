239eb4917196cbcd9f8b467199d0f26e183c1afc
dkbozkurt <d.k.bozkurt@gmail.com>
2022-01-09 21:13:21 +0300

    Movement scripts with Joysticks
    Scripts to move character on X-Z plane.

---
+++ JoystickMovementScripts/3_CenterMass+MyJoystick/CharacterControlManager.cs
@@ -0,0 +1,86 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Type 3
+
+/// <summary>
+/// Attach this script to parent object of the player.
+/// 
+/// This script works with "ManagerJoystick.cs" script that attached to the joystick that is handmade(not from the store).
+/// 
+///  Important Note: Freeze position and Freeze rotations X,Y,Z components should be checked for the child object(player),
+/// </summary>
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControlManager : MonoBehaviour
+{
+    //
+    private Vector3 v_movement;
+    private CharacterController _characterController;
+    public float moveSpeed;
+    
+    //
+    private Transform meshPlayer;
+    private float gravity;
+    
+    //
+    [SerializeField]private ManagerJoystick _managerJoystick;
+    private float inputX;
+    private float inputZ;
+    
+    //
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        _characterController = gameObject.GetComponent<CharacterController>();
+        moveSpeed = 0.1f;
+        gravity = 0.5f;
+        rotationSpeed = 1f;
+
+        meshPlayer = gameObject.transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        //inputX = Input.GetAxis("Horizontal");
+        //inputZ = Input.GetAxis("Vertical");
+        
+        inputX  =_managerJoystick.inputHorizontal();
+        inputZ = _managerJoystick.inputVertical();
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // gravity
+        if (_characterController.isGrounded)
+        {
+            v_movement.y = 0f;
+        }
+        else
+        {
+            v_movement.y -= gravity * Time.deltaTime;
+        }
+        
+        // Movement
+        v_movement = new Vector3(inputX*moveSpeed, 0, inputZ*moveSpeed);
+        _characterController.Move(v_movement);
+        
+        // mesh rotate
+        if (inputX != 0 || inputZ != 0)
+        {
+            Vector3 lookDir = new Vector3(v_movement.x*rotationSpeed, v_movement.y, v_movement.z*rotationSpeed);
+            meshPlayer.rotation = Quaternion.LookRotation(lookDir);    
+        }
+        
+        
+        
+    }
+}
---
+++ JoystickMovementScripts/3_CenterMass+MyJoystick/ManagerJoystick.cs
@@ -0,0 +1,91 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+// Type 3
+
+/// <summary>
+/// Attach this script to "JoystickBackGround" object which should be prepared by you.(Basic Joystick)
+/// 
+/// "imgJoystickBg" should refer to background of joystick. Should be parent object and this script must add as a component.
+/// "imgJoystickCenter" should refer to center dot of the jotstick and should be child object of "imgJoystickBg"
+/// 
+/// </summary>
+
+public class ManagerJoystick : MonoBehaviour, IDragHandler, IPointerDownHandler,IPointerUpHandler
+{
+    private Image imgJoystickBg;
+    private Image imgJoystickCenter;
+    private Vector2 posInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imgJoystickBg = gameObject.GetComponent<Image>();
+        imgJoystickCenter = gameObject.transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(imgJoystickBg.rectTransform, 
+            eventData.position,
+            eventData.pressEventCamera,
+            out posInput))
+        {
+            posInput.x = posInput.x / (imgJoystickBg.rectTransform.sizeDelta.x);
+            posInput.y = posInput.y / (imgJoystickBg.rectTransform.sizeDelta.y);
+            // Debug.Log(posInput.x.ToString()+'/'+posInput.y.ToString());
+            
+            //normalize
+
+            if (posInput.magnitude > 1.0f)
+            {
+                posInput = posInput.normalized;
+            }
+            
+            //joystick move
+            imgJoystickCenter.rectTransform.anchoredPosition = new Vector2(
+                posInput.x * (imgJoystickBg.rectTransform.sizeDelta.x/2),
+                posInput.y * (imgJoystickBg.rectTransform.sizeDelta.y/2));
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        posInput = Vector2.zero;
+        imgJoystickCenter.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    public float inputHorizontal()
+    {
+        if (posInput.x != 0)
+        {
+            return posInput.x;
+        }
+        else
+            return Input.GetAxis("Horizontal");
+    }
+
+    public float inputVertical()
+    {
+        if (posInput.y != 0)
+        {
+            return posInput.y;
+        }
+        else
+        {
+            return Input.GetAxis("Vertical");
+        }
+    }
+    
+}
