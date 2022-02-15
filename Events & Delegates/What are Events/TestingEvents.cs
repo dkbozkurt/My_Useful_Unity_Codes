9c253aa1fefabc96b04bd9b455e43616942a7908
dkbozkurt <d.k.bozkurt@gmail.com>
2022-02-15 22:53:36 +0300

    TestingEvents with Publisher and Subscriber

--- Events & Delegates/What are Events/TestingEvents.cs
+++ Events & Delegates/What are Events/TestingEvents.cs
@@ -18,7 +18,7 @@ using UnityEngine.Events;
 
 
 // PUBLISHER SCRIPT
-public class TestingEventsPublisher : MonoBehaviour
+public class TestingEvents : MonoBehaviour
 {
     // Version 1
     // public event EventHandler OnSpacePressed; // Event's format generally start with On.    
