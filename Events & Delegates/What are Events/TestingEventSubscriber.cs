7be4edd3d355577f1d461e1556707cd0f1ce0b0a
dkbozkurt <d.k.bozkurt@gmail.com>
2022-02-15 22:49:41 +0300

    TestingEvents with Publisher and Subscriber

---
+++ Events & Delegates/What are Events/TestingEventSubscriber.cs
@@ -0,0 +1,67 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// What are Events?
+/// 
+/// /// NOTE : The subscriber script that is expected to work,
+/// must be attached to the GameObject that, the Publisher script is attached.
+/// </summary>
+
+
+// SUBSCRIBER SCRIPT
+public class TestingEventSubscriber : MonoBehaviour
+{
+    // Version 1
+    // void Start()
+    // {
+    //     TestingEvents testingEvents = GetComponent<TestingEvents>();
+    //     testingEvents.OnSpacePressed += TestingEvents_OnSpacePressed;
+    // }
+    //
+    // private void TestingEvents_OnSpacePressed(object sender, EventArgs e)
+    // {
+    //     Debug.Log("Space by subscriber");
+    //     TestingEvents testingEvents = GetComponent<TestingEvents>();
+    //     testingEvents.OnSpacePressed -= TestingEvents_OnSpacePressed;
+    // }
+    
+    // Version 2
+    void Start()
+    {
+        TestingEvents testingEvents = GetComponent<TestingEvents>();
+        testingEvents.OnSpacePressed += TestingEvents_OnSpacePressed;
+
+        testingEvents.OnFloatEvent += TestingEvents_OnFloatEvent;
+
+        testingEvents.OnActionEvent += TestingEvents_OnActionEvent;
+        
+    }
+    
+    private void TestingEvents_OnSpacePressed(object sender, TestingEvents.OnSpacePressedEventArgs e)
+    {
+        Debug.Log("Space by subscriber" + e.spaceCount);
+        TestingEvents testingEvents = GetComponent<TestingEvents>();
+        testingEvents.OnSpacePressed -= TestingEvents_OnSpacePressed;
+    }
+
+    private void TestingEvents_OnFloatEvent(float f)
+    {
+        Debug.Log("Flott: "+ f);
+    }
+
+    private void TestingEvents_OnActionEvent(bool arg1, int arg2)
+    {
+        Debug.Log("Arg1: "+ arg1 +  " Arg2: "+ arg2);
+    }
+
+    public void TestingUnityEvent()
+    {
+        Debug.Log("TestingUnityEvent");
+    }
+}
---
+++ Events & Delegates/What are Events/TestingEvents.cs
@@ -0,0 +1,93 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+/// <summary>
+/// What are Events?
+/// 
+/// NOTE : The subscriber script that is expected to work,
+/// must be attached to the GameObject that, the Publisher script is attached.
+/// referance: https://www.youtube.com/watch?v=OuZrhykVytg
+/// </summary>
+
+
+// PUBLISHER SCRIPT
+public class TestingEvents : MonoBehaviour
+{
+    // Version 1
+    // public event EventHandler OnSpacePressed; // Event's format generally start with On.    
+    //
+    // private void Start()
+    // {
+    //     
+    // }
+    //
+    // private void Update()
+    // {
+    //     if(Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         // Space pressed!
+    //         // if (OnSpacePressed != null)
+    //         // {
+    //         //     OnSpacePressed(this,EventArgs.Empty);
+    //         // }
+    //         
+    //         // Short way to do.
+    //         OnSpacePressed?.Invoke(this,EventArgs.Empty);
+    //
+    //     }
+    // }
+    
+    // Version 2
+    
+    // Event with parameters. (Classical usage)
+    public event EventHandler<OnSpacePressedEventArgs> OnSpacePressed;
+    public class OnSpacePressedEventArgs : EventArgs
+    {
+        public int spaceCount;
+    }
+    
+    // Delegate
+    public delegate void TestEventDelegate(float f);
+    public event TestEventDelegate OnFloatEvent;
+    
+    // Action
+    public event Action<bool, int> OnActionEvent;
+    
+    // UnityEvent
+    public UnityEvent OnUnityEvent;
+    
+    
+    
+    private int spaceCount;
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            OnSpacePressed?.Invoke(this,new OnSpacePressedEventArgs{ spaceCount = spaceCount });
+            
+            OnFloatEvent?.Invoke(5.5f);
+            
+            OnActionEvent?.Invoke(true,56);
+            
+            // Must be assigned through inspector.
+            OnUnityEvent?.Invoke();
+    
+        }
+    }
+
+    
+}
