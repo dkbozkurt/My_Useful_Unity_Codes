49685c43d5af38159e95530289187da92feb6165
dkbozkurt <dkaanbozkurt@gmail.com>
2023-02-06 16:15:55 +0300

    TriggerArea fill

---
+++ TriggerArea_Fill/Scripts/InteractableBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+namespace CpiTemplate.Game.Creative.Scripts.Behaviours
+{
+    public abstract class InteractableBase<T> : MonoBehaviour
+    {
+        [SerializeField] protected bool _triggerOneTime = false;
+        [SerializeField] protected bool _destroyAfterTrigger = false;
+        [SerializeField] private bool _searchParentToo = false;
+        
+        protected bool _isTriggered = false;
+
+        public void ResetInteractable()
+        {
+            _isTriggered = false;
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if(_isTriggered ) return;
+
+            T t = other.GetComponent<T>();
+
+            if (t == null)
+            {
+                if(!_searchParentToo) return;
+
+                t = other.GetComponentInParent<T>();
+                if(t == null) return;
+            }
+
+            if (_triggerOneTime) _isTriggered = true;
+            
+            TriggerEnter(t);
+            
+            if(!_destroyAfterTrigger) return;
+
+            _isTriggered = true;
+            Destroy(gameObject);
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            T t = other.GetComponent<T>();
+            if (t == null)
+            {
+                if (!_searchParentToo)
+                    return;
+
+                t = other.GetComponentInParent<T>();
+                if (t == null)
+                    return;
+            }
+
+            TriggerExit(t);
+        }
+        
+        public abstract void TriggerEnter(T t);
+        public abstract void TriggerExit(T t);
+        
+    }
+}
---
+++ TriggerArea_Fill/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CpiTemplate.Game.Playable.Scripts.Controllers;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CpiTemplate.Game.Creative.Scripts.Behaviours
+{
+    public class TriggerBehaviour : InteractableBase<CharacterMovementController>
+    {
+        public static event Action OnTriggerOver;
+
+        [SerializeField] private Image _fillImage;
+        [SerializeField] private float _fillDuration;
+        [SerializeField] private Collider _collider;
+
+        private Canvas _canvas;
+        private bool _isPlayerTriggered = false;
+
+        private void Awake()
+        {
+            _canvas = GetComponentInChildren<Canvas>();
+            Toggle(true);
+        }
+
+        private void OnDisable()
+        {
+            _fillImage.DOKill();
+        }
+
+        public void Toggle(bool on)
+        {
+            _canvas.enabled = on;
+            _collider.enabled = on;
+        }
+
+        public override void TriggerEnter(CharacterMovementController t)
+        {
+            if(_isPlayerTriggered) return;
+            
+            _isPlayerTriggered = true;
+            
+            _fillImage.DOKill();
+            _fillImage.DOFillAmount(1, _fillDuration).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                OnTriggerOver?.Invoke();
+                Debug.Log("Trigger finished");
+                ResetTrigger();
+            });
+        }
+
+        public override void TriggerExit(CharacterMovementController t)
+        {
+            _isPlayerTriggered = false;
+            RewindTrigger();
+        }
+
+        private void RewindTrigger()
+        {
+            _fillImage.DOKill();
+            _fillImage.DOFillAmount(0, _fillDuration / 3f).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                _fillImage.fillAmount = 0f;
+            });
+        }
+
+        private void ResetTrigger()
+        {
+            _fillImage.DOKill();
+            _fillImage.fillAmount = 0f;
+        }
+    }
+}
