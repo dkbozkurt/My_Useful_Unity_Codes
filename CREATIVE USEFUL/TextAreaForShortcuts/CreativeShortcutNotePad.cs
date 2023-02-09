2e545f481472fce6d48c233f0a4ea478c76b5626
dkbozkurt <dkaanbozkurt@gmail.com>
2023-02-09 10:29:55 +0300

    CreativeShortcutNotePad ~TextArea

---
+++ CREATIVE USEFUL/TextAreaForShortcuts/CreativeShortcutNotePad.cs
@@ -0,0 +1,17 @@
+﻿// Dogukan Kaan Bozkurt
+//      github.com/dkbozkurt
+
+using UnityEngine;
+
+namespace CpiTemplate.Game.Creative.Scripts.Managers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CreativeShortcutNotePad : MonoBehaviour
+    {
+        // [Multiline]
+        [TextArea(minLines: 30, maxLines :50)]
+        [SerializeField] private string _notes;
+    }
+}
