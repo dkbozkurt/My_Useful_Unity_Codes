cf708f054cc2f1e5232924c02f2b9fe11da7346d
dkbozkurt <dkaanbozkurt@gmail.com>
2023-02-12 22:16:53 +0300

    Joystick movement trivial

--- JoystickMovementScripts/Handmade Joystick Controllers/Floating Joystick/JoystickManager.cs
+++ JoystickMovementScripts/Handmade Joystick Controllers/Floating Joystick/JoystickManager.cs
@@ -1,87 +1,78 @@
-// Dogukan Kaan Bozkurt
-//      github.com/dkbozkurt
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using CpiTemplate.Game.Playable.Scripts.Helpers;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-/// <summary>
-/// Floating Handmade Joystick
-///
-/// Attach this script onto "JoystickSurface" gameobject that cover all of the screen and includes
-/// "JoystickBackground" as a child object which also has "JoystickHande" as a child object.
-/// 
-/// Send return values of inputHorizontal and inputVertical methods into expected place. 
-/// </summary>
-
-public class JoystickManager : MonoBehaviour, IDragHandler, IPointerDownHandler,IPointerUpHandler
+namespace CpiTemplate.Game.Playable.Scripts.Managers
 {
-    public RectTransform joystickBackground;
-    public RectTransform joystickHandle;
+    public class JoystickManager : SingletonBehaviour<JoystickManager>,IDragHandler, IPointerDownHandler,IPointerUpHandler
+    {
+         public RectTransform joystickBackground;
+        public RectTransform joystickHandle;
     
-    private Vector2 _input = Vector2.zero;
-    private Vector2 _joyPosition = Vector2.zero;
+        private Vector2 _input = Vector2.zero;
+        private Vector2 _joyPosition = Vector2.zero;
 
-    private float _thresholdValue = 0.2f;
+        private float _thresholdValue = 0.2f;
     
-    public void OnDrag(PointerEventData eventData)
-    {
-        Vector2 joyDirection = eventData.position - _joyPosition;
-        _input = (joyDirection.magnitude > joystickBackground.sizeDelta.x / 2f)
-            ? joyDirection.normalized
-            : joyDirection / (joystickBackground.sizeDelta.x / 2f);
-        joystickHandle.anchoredPosition = (_input * joystickBackground.sizeDelta.x / 2f) * 1f;
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector2 joyDirection = eventData.position - _joyPosition;
+            _input = (joyDirection.magnitude > joystickBackground.sizeDelta.x / 2f)
+                ? joyDirection.normalized
+                : joyDirection / (joystickBackground.sizeDelta.x / 2f);
+            joystickHandle.anchoredPosition = (_input * joystickBackground.sizeDelta.x / 2f) * 1f;
 
-    }
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        joystickBackground.gameObject.SetActive(true);
-        OnDrag(eventData);
-        _joyPosition = eventData.position;
-        joystickBackground.position = eventData.position;
-        joystickHandle.anchoredPosition = Vector2.zero;
-    }
+        }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            joystickBackground.gameObject.SetActive(true);
+            OnDrag(eventData);
+            _joyPosition = eventData.position;
+            joystickBackground.position = eventData.position;
+            joystickHandle.anchoredPosition = Vector2.zero;
+        }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        joystickBackground.gameObject.SetActive(false);
-        _input=Vector2.zero;
-        joystickHandle.anchoredPosition = Vector2.zero;
-    }
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            joystickBackground.gameObject.SetActive(false);
+            _input=Vector2.zero;
+            joystickHandle.anchoredPosition = Vector2.zero;
+        }
     
 
-    public float inputHorizontal()
-    {
+        public float InputHorizontal()
+        {
         
-        if (_input.x != 0)
-        {
-            if (_input.x > _thresholdValue || _input.x < -_thresholdValue) return _input.x;
-            else return 0f;
-        }
-        else
-        {
-            _input.x = 0f;
-            return Input.GetAxis("Horizontal");
-        }
+            if (_input.x != 0)
+            {
+                if (_input.x > _thresholdValue || _input.x < -_thresholdValue) return _input.x;
+                else return 0f;
+            }
+            else
+            {
+                _input.x = 0f;
+                return Input.GetAxis("Horizontal");
+            }
             
-    }
+        }
 
-    public float inputVertical()
-    {
+        public float InputVertical()
+        {
         
-        if (_input.y!= 0)
-        {
-            if (_input.y > _thresholdValue || _input.y < -_thresholdValue) return _input.y;
-            else return 0f;
+            if (_input.y!= 0)
+            {
+                if (_input.y > _thresholdValue || _input.y < -_thresholdValue) return _input.y;
+                else return 0f;
+            }
+            else
+            {
+                _input.y = 0f;
+                return Input.GetAxis("Vertical");
+            }
         }
-        else
+
+        protected override void OnAwake()
         {
-            _input.y = 0f;
-            return Input.GetAxis("Vertical");
         }
     }
-
 }
