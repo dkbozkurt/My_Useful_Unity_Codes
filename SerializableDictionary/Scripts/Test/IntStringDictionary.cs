69d23b2c197b7a22c6310eb32a2dee9cccd78960
dkbozkurt <dkaanbozkurt@gmail.com>
2023-02-27 16:36:05 +0300

    Serializable Dictionary & Serializable Hash Set

---
+++ SerializableDictionary/Scripts/Test/IntStringDictionary.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+using SerializableDictionary.Scripts.Helpers;
+
+namespace SerializableDictionary.Scripts.Test
+{
+    [Serializable]
+    public class IntStringDictionary : SerializableDictionary<int,string> { }
+}
---
+++ SerializableDictionary/Scripts/Test/SerializableDictionaryTest.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SerializableDictionary.Scripts.Test
+{
+    public class SerializableDictionaryTest : MonoBehaviour
+    {
+        [SerializeField] private IntStringDictionary _intStringDictionary;
+    }
+}
