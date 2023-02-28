d8f9ce2835edc78338365573da0631ae42259c0a
dkbozkurt <dkaanbozkurt@gmail.com>
2023-02-28 23:30:39 +0300

    Assets to must have updated

---
+++ Assets to Must Have/EpicVictoryEffects/Readme.txt
@@ -0,0 +1,12 @@
+Epic Victory Effects
+Mixture Art
+Stylized and unique victory particle effects. 
+Easy to use, Customize very easily. 
+Works with 2D and 3D games. 
+Suitable for mobile and desktop. 
+
+Manual
+https://drive.google.com/open?id=0B79ks3rxgvWzVFduMXJSY3d0VUk
+
+.APK
+https://drive.google.com/open?id=0B79ks3rxgvWzTHZpdjU0emFrMFU
---
+++ Assets to Must Have/EpicVictoryEffects/Scripts/starFxController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starFxController : MonoBehaviour {
+
+	public GameObject[] starFX;
+	public int ea;
+	public int currentEa;
+	public float delay;
+	public float currentDelay;
+	public bool isEnd;
+	public int idStar;
+	public static starFxController myStarFxController;
+
+	void Awake () {
+		myStarFxController = this;
+	}
+
+	void Start () {
+		Reset ();
+	}
+
+	void Update () {
+		if (!isEnd) {
+			currentDelay -= Time.deltaTime;
+			if (currentDelay <= 0) {
+				if (currentEa != ea) {
+					currentDelay = delay;
+					starFX [currentEa].SetActive (true);
+					currentEa++;
+				} else {
+					isEnd = true;
+					currentDelay = delay;
+					currentEa = 0;
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Reset ();
+		}
+	}
+
+	public void Reset () {
+		for (int i = 0; i < 3; i++) {
+			starFX [i].SetActive (false);
+		}
+		currentDelay = delay;
+		currentEa = 0;
+		isEnd = false;
+		for (int i = 0; i < 3; i++) {
+			starFX [i].SetActive (false);
+		}
+	}
+}
