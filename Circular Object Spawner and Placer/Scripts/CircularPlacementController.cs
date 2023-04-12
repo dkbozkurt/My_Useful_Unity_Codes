bd60d9d3ede15d5ec6e079982f4804b0eccef7fc
Doğukan Bozkurt <dkaanbozkurt@gmail.com>
2023-04-12 17:20:14 +0300

    Spacing degree added

--- Circular Object Spawner and Placer/Scripts/CircularPlacementController.cs
+++ Circular Object Spawner and Placer/Scripts/CircularPlacementController.cs
@@ -19,6 +19,7 @@ namespace Circular_Object_Spawner_and_Placer.Scripts
         [SerializeField] private int _objectToSpawnCount = 6;
         [SerializeField] private float _circleRadius = 2f;
         [SerializeField] private float _drawDegree = 360f;
+        [SerializeField] private float _spacingDegree=0f;
         
         private List<GameObject> _spawnedObjects = new List<GameObject>();
 
@@ -32,26 +33,26 @@ namespace Circular_Object_Spawner_and_Placer.Scripts
         
         private void Test()
         {
-            CircularSpawnNOrder(_objectToSpawnPrefab,_objectToSpawnCount,_drawDegree,_circleRadius);
+            CircularSpawnNOrder(_objectToSpawnPrefab,_objectToSpawnCount,_drawDegree,_circleRadius,_spacingDegree);
         }
 
-        public void CircularSpawnNOrder<T>(T objectClass , int objectAmount, float drawDegree = 360f, float circleRadius = 2f) where T : MonoBehaviour
+        public void CircularSpawnNOrder<T>(T objectClass , int objectAmount, float drawDegree = 360f, float circleRadius = 2f,float spacingDegree = 0f) where T : MonoBehaviour
         {
-            CircularSpawnNOrder(objectClass.gameObject, objectAmount, circleRadius, drawDegree);
+            CircularSpawnNOrder(objectClass.gameObject, objectAmount, circleRadius, drawDegree,spacingDegree);
         }
         
-        public void CircularSpawnNOrder(Transform objectPrefabTransform,int objectAmount, float drawDegree = 360f,float circleRadius = 2f)
+        public void CircularSpawnNOrder(Transform objectPrefabTransform,int objectAmount, float drawDegree = 360f,float circleRadius = 2f,float spacingDegree = 0f)
         {
-            CircularSpawnNOrder(objectPrefabTransform.gameObject, objectAmount, circleRadius, drawDegree);
+            CircularSpawnNOrder(objectPrefabTransform.gameObject, objectAmount, circleRadius, drawDegree,spacingDegree);
         }
 
-        public void CircularSpawnNOrder(GameObject objectPrefab,int objectAmount, float drawDegree = 360f,float circleRadius = 2f)
+        public void CircularSpawnNOrder(GameObject objectPrefab,int objectAmount, float drawDegree = 360f,float circleRadius = 2f,float spacingDegree = 0f)
         {
             RemoveSpawnedObjects();
             
             for (int i = 0; i < objectAmount; i++)
             {
-                float angle = (i * Mathf.PI * (drawDegree/180f)) / objectAmount;
+                float angle = (Mathf.PI * spacingDegree/180f) + (i * Mathf.PI * (drawDegree/180f)) / objectAmount;
                 Vector3 newPos = new Vector3(Mathf.Cos(angle) * circleRadius, 0, Mathf.Sin(angle)*circleRadius);
                 GameObject go = Instantiate(objectPrefab, newPos, Quaternion.identity);
                 _spawnedObjects.Add(go);
@@ -59,47 +60,47 @@ namespace Circular_Object_Spawner_and_Placer.Scripts
         }
         
         public void CircularOrder<T>(T[] objectsToOrder, float drawDegree = 360f,
-            float circleRadius = 2f) where T : MonoBehaviour
+            float circleRadius = 2f,float spacingDegree = 0f) where T : MonoBehaviour
         {
             Transform[] objectsArray = objectsToOrder.Select(obj => obj.transform).ToArray();
             
-            CircularOrder(objectsArray,drawDegree,circleRadius);
+            CircularOrder(objectsArray,drawDegree,circleRadius,spacingDegree);
         }
         
         public void CircularOrder<T>(List<T> objectsToOrder, float drawDegree = 360f,
-            float circleRadius = 2f) where T : MonoBehaviour
+            float circleRadius = 2f,float spacingDegree = 0f) where T : MonoBehaviour
         {
             Transform[] objectsArray = objectsToOrder.Select(obj => obj.transform).ToArray();
             
-            CircularOrder(objectsArray,drawDegree,circleRadius);
+            CircularOrder(objectsArray,drawDegree,circleRadius,spacingDegree);
         }
 
         public void CircularOrder(List<GameObject> objectsToOrder, float drawDegree = 360f,
-            float circleRadius = 2f)
+            float circleRadius = 2f,float spacingDegree = 0f)
         {
             Transform[] objectsArray = objectsToOrder.Select(obj => obj.transform).ToArray();
             
-            CircularOrder(objectsArray,drawDegree,circleRadius);
+            CircularOrder(objectsArray,drawDegree,circleRadius,spacingDegree);
         }
         
         public void CircularOrder(GameObject[] objectsToOrder, float drawDegree = 360f,
-            float circleRadius = 2f)
+            float circleRadius = 2f,float spacingDegree = 0f)
         {
             Transform[] objectsArray = objectsToOrder.Select(obj => obj.transform).ToArray();
             
-            CircularOrder(objectsArray,drawDegree,circleRadius);
+            CircularOrder(objectsArray,drawDegree,circleRadius,spacingDegree);
         }
 
-        public void CircularOrder(List<Transform> objectsToOrder, float drawDegree = 360f,float circleRadius = 2f)
+        public void CircularOrder(List<Transform> objectsToOrder, float drawDegree = 360f,float circleRadius = 2f,float spacingDegree = 0f)
         {
-            CircularOrder(objectsToOrder.ToArray(),drawDegree,circleRadius);
+            CircularOrder(objectsToOrder.ToArray(),drawDegree,circleRadius,spacingDegree);
         }
         
-        public void CircularOrder(Transform[] objectsToOrder, float drawDegree = 360f, float circleRadius = 2f)
+        public void CircularOrder(Transform[] objectsToOrder, float drawDegree = 360f, float circleRadius = 2f,float spacingDegree = 0f)
         {
             for (int i = 0; i < objectsToOrder.Length; i++)
             {
-                float angle = (i * Mathf.PI * (drawDegree/180f)) / objectsToOrder.Length;
+                float angle = (Mathf.PI * (spacingDegree/180f)) + (i * Mathf.PI * (drawDegree/180f)) / objectsToOrder.Length;
                 Vector3 newPos = new Vector3(Mathf.Cos(angle) * circleRadius, 1, Mathf.Sin(angle)*circleRadius);
                 objectsToOrder[i].position = newPos;
             }
